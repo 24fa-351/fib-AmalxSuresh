f5e1db9829b21e1d727f20b1dbf43a382f86f75d
AmalxSuresh <amalsuresh@csu.fullerton.edu>
2024-09-03 22:52:05 -0700

    initial user inputs

--- fib.c
+++ fib.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-   printf("Hello!\n");
+   int user_input;
+   char fibonacci_method[1];
+   char file_name[30];
+
+   printf("Enter and integer > ");
+   scanf("%d", &user_input);
+   printf("\nEnter 'i' for iterative and 'r' for recursive > ");
+   scanf("%s", fibonacci_method);
+   printf("\nEnter a file name > ");
+   scanf("%s", file_name);
+
    return 0;
 }
